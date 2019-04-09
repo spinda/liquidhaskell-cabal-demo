8d6374edaa1103821bcdb15144ccabb5361a5562
Michael Smith <michael@spinda.net>
2019-04-08 17:24:52 -0700

    Tidy things up a bit

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,12 @@
-
 import Distribution.Simple
 import LiquidHaskell.Cabal
 
-data Choice = Simple | Post 
+data Choice = Simple | Post
 
-choice :: Choice 
+choice :: Choice
 choice = Post -- Simple
 
-main :: IO () 
-main = case choice of 
-         Simple -> liquidHaskellMain 
+main :: IO ()
+main = case choice of
+         Simple -> liquidHaskellMain
          Post   -> liquidHaskellMainHooks
